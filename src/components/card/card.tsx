88909bda7d5c61743ec2a48c45106b86d4db32d1
Nathan Jenner <137852143+NathanJenner@users.noreply.github.com>
2025-05-20 08:36:25 +1000

    updating list array with projects

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -3,44 +3,74 @@ import { Card, CardBody, CardFooter, Image } from "@heroui/react";
 export default function CardBase() {
   const list = [
     {
-      title: "Orange",
+      title: "Math Game",
       img: "/images/fruit-1.jpeg",
-      price: "$5.50",
+      desc: "xzx",
     },
     {
-      title: "Tangerine",
+      title: "Calculator",
       img: "/images/fruit-2.jpeg",
-      price: "$3.00",
+      desc: "xzx",
     },
     {
-      title: "Raspberry",
+      title: "Habit Logger",
       img: "/images/fruit-3.jpeg",
-      price: "$10.00",
+      desc: "xzx",
     },
     {
-      title: "Lemon",
+      title: "Coding Tracker",
       img: "/images/fruit-4.jpeg",
-      price: "$5.30",
+      desc: "xzx",
     },
     {
-      title: "Avocado",
+      title: "Flashcards",
       img: "/images/fruit-5.jpeg",
-      price: "$15.70",
+      desc: "xzx",
     },
     {
-      title: "Lemon 2",
+      title: "Drinks Info API",
       img: "/images/fruit-6.jpeg",
-      price: "$8.00",
+      desc: "xzx",
     },
     {
-      title: "Banana",
+      title: "Phone Book App",
       img: "/images/fruit-7.jpeg",
-      price: "$7.50",
+      desc: "xzx",
     },
     {
-      title: "Watermelon",
+      title: "Shifts Logger",
       img: "/images/fruit-8.jpeg",
-      price: "$12.20",
+      desc: "xzx",
+    },
+    {
+      title: "Exercise Tracker",
+      img: "/images/fruit-5.jpeg",
+      desc: "xzx",
+    },
+    {
+      title: "Sports Notifier",
+      img: "/images/fruit-6.jpeg",
+      desc: "xzx",
+    },
+    {
+      title: "Excel Reader",
+      img: "/images/fruit-7.jpeg",
+      desc: "xzx",
+    },
+    {
+      title: "Unit Testing",
+      img: "/images/fruit-8.jpeg",
+      desc: "xzx",
+    },
+    {
+      title: "Drink Tracker",
+      img: "/images/fruit-7.jpeg",
+      desc: "xzx",
+    },
+    {
+      title: "Movie Catalog",
+      img: "/images/fruit-8.jpeg",
+      desc: "xzx",
     },
   ];
 
@@ -66,7 +96,7 @@ export default function CardBase() {
           </CardBody>
           <CardFooter className="text-small justify-between">
             <b>{item.title}</b>
-            <p className="text-default-500">{item.price}</p>
+            <p className="text-default-500">{item.desc}</p>
           </CardFooter>
         </Card>
       ))}
