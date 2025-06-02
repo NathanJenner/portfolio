c6912ebcf43a3f0a4b71e3a1b3e83c666d1de325
Nathan Jenner <137852143+NathanJenner@users.noreply.github.com>
2025-06-02 20:04:50 +1000

    Removed GitHub and Documentation section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,28 +56,7 @@ export default function IndexPage() {
           </div>
         </section>
 
-        <section>
-          <div className="flex gap-3">
-            <Link
-              isExternal
-              className={buttonStyles({
-                color: "primary",
-                radius: "full",
-                variant: "shadow",
-              })}
-            >
-              Documentation
-            </Link>
-            <Link
-              isExternal
-              className={buttonStyles({ variant: "bordered", radius: "full" })}
-              href={siteConfig.links.github}
-            >
-              <GithubIcon size={20} />
-              GitHub
-            </Link>
-          </div>
-        </section>
+        
 
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
           <div className="inline-block max-w-lg text-center justify-center">
